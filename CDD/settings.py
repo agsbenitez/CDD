cf9e8856ad9809f04a74c6b6e173debbca164f80
Ariel Benitez <agsbenitez@gmail.com>
2023-06-12 11:15:24 -0300

    para render sin tail

--- CDD/settings.py
+++ CDD/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ  
+
+#variable de entorno
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
